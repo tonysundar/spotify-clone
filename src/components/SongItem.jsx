a6e3045911fd563b152041e6cef65f5832c41c8a
tonysundar <suryajyothi124@gmail.com>
2024-10-13 16:51:29 +0530

    descRemove

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -9,7 +9,7 @@ const SongItem = ({name,image,desc,id}) => {
     <div onClick={()=>playWithId(id)} className='min-2-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
       <img className='rounded' src={image} alt=''/>
       <p className='font-bold mt-2 mb-1'>{name}</p>
-      <p className='text-slate-200 text-sm'>{desc}</p>
+      <p className='text-slate-200 text-sm'></p>
     </div>
   )
 }
