6696d5d09e522f2dbb6cedbe0e551450eb187f05
tonysundar <suryajyothi124@gmail.com>
2024-10-13 16:10:14 +0530

    spotify

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import Sidebar from './components/sidebar.jsx'
+import Player from './components/Player.jsx'
+import Display from './components/Display.jsx'
+import { PlayerContext } from './context/PlayerContext.jsx'
+
+const App = () => {
+
+  const {audioRef,track} = useContext(PlayerContext)
+
+  return (
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <Sidebar />
+        <Display />
+       </div>
+          <Player/>  
+          <audio ref={audioRef} src={track.file} preload='auto'></audio>                                                                                                                                 
+    </div>
+  )
+}
+
+export default App
